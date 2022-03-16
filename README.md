34d3f7289e7b46aa741b7766c44d6003ce5bd6d2
StacyChebet <staceychebet23@gmail.com>
2022-03-17 00:10:59 +0300

    My first c project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the repo containing practice files on low level programming
---
+++ main.c
@@ -0,0 +1,16 @@
+# 942 "/usr/include/stdio.h" 3 4
+
+# 2 "main.c" 2
+
+# 3 "main.c"
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    return (0);
+}
